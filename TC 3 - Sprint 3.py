dbde495704773aaa2024fcfd07c500b8c4f513ec
virginiafarias44 <84880710+virginiafarias44@users.noreply.github.com>
2022-12-17 18:13:28 -0300

    Add files via upload

---
+++ TC 3 - Sprint 3.py
@@ -0,0 +1,35 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+option1 = Options()
+option1.add_argument("--disable-notifications")
+option1.add_argument("--start-maximized")
+
+driver = webdriver.Chrome(chrome_options=option1)
+driver.get("https://shop.samsung.com/ar/")
+
+time.sleep(5)
+
+cp = driver.find_element(By.XPATH, "/html/body/div[3]/div[1]/div/div/section/div[1]/button")
+cp.click()
+time.sleep(5)
+
+cookies = driver.find_element(By.XPATH, '//*[@id="truste-consent-button"]').click()
+time.sleep(8)
+
+try:
+    tv_y_audio =  driver.find_element(By.LINK_TEXT, 'TV & Audio').click()
+except:
+    print("Not working")
+
+time.sleep(10)
+
+try:
+    driver.find_element(By.XPATH, '//*[@id="headerSamsung"]/div[2]/div/div/a').click()
+except:
+    print("Not return")
+
+time.sleep(10)
